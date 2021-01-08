8d49d915d3d8d1252ddd1ae17494e33300ab0c4e
sweth <sweth@192.168.1.129>
2021-01-08 17:21:41 -0500

    First commit...

---
+++ src/TestDemo/TestClass.java
@@ -0,0 +1,25 @@
+package TestDemo;
+
+public class TestClass {
+	
+	public static void main(String[] args) {
+		int x = 50;
+		int y = 50;
+		if
+		 (x==y)
+		{
+
+		  System.out.println("1");
+		} 
+		else if
+		 (x > y) {
+		  System.out.println("2");
+		} 
+		else
+		 {
+		  System.out.println("3");
+		}  
+	}
+	
+
+}
